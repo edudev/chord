23472997ac0adb0dfa90a5913f465299210a1ff6
R. Serkan Keskin <rs.keskin@student.vu.nl>
2019-11-19 18:18:33 +0100

    Added memslap. (It's 💩 tho)i

--- test-scripts/install.sh
+++ test-scripts/install.sh
@@ -2,6 +2,7 @@
 
 MEMCACHED_VER="1.5.20"
 MEMTIER_B_VER="1.2.17"
+LMEMCACHED_VER="1.0.18"
 
 mkdir -p $HOME/opt/
 
@@ -13,7 +14,7 @@ cd memcached-$MEMCACHED_VER/
 	make &&
 	make test &&
 	make install
-cd ..
+cd $HOME
 rm -rf memcached-$MEMCACHED_VER/
 rm memcached-$MEMCACHED_VER.tar.gz
 
@@ -25,7 +26,17 @@ autoreconf -ivf &&
 	./configure --prefix=$HOME/opt/ &&
 	make &&
 	make install-exec
-cd ..
+cd $HOME
 rm -rf memtier_benchmark-$MEMTIER_B_VER/
 rm $MEMTIER_B_VER.tar.gz
 
+cd $HOME
+wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-$LMEMCACHED_VER.tar.gz
+tar -zxf libmemcached-$LMEMCACHED_VER.tar.gz
+cd libmemcached-$LMEMCACHED_VER/
+./configure --prefix=$HOME/opt/ &&
+	make &&
+	make install
+cd $HOME
+rm -rf libmemcached-$LMEMCACHED_VER/
+rm libmemcached-$LMEMCACHED_VER.tar.gz
--- test-scripts/test.sh
+++ test-scripts/test.sh
@@ -21,7 +21,14 @@ MEMTIER_B_EXEC="$HOME/opt/bin/memtier_benchmark"
 MEMTIER_B_ARGS="-s $NODE -p 11211 -P memcache_text -x 1"
 echo "Running Memtier Benchmark..."
 prun -np 1 $MEMTIER_B_EXEC $MEMTIER_B_ARGS </dev/null >$HOME/memtier_out_$TIMESTAMP 2>&1
-echo "Done!"
 echo "Benchmark results: $HOME/memtier_out_$TIMESTAMP"
+
+MEMSLAP_EXEC="$HOME/opt/bin/memslap"
+MEMSLAP_ARGS="--servers=$NODE"
+echo "Running Memslap..."
+prun -np 1 $MEMSLAP_EXEC $MEMSLAP_ARGS </dev/null >$HOME/memslap_out_$TIMESTAMP 2>&1
+echo "Benchmark results: $HOME/memslap_out_$TIMESTAMP"
+
+echo "Done!"
 
 rm $HOME/tmpout
