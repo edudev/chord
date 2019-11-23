30fca0561faaf3d6b4590f20cdaacc76983b39b1
dcasenove <d.casenove@gmail.com>
2019-11-23 21:59:46 +0100

    Add local script

---
+++ local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir remote
+cp test-scripts/* remote/
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Getting /x/sys/unix"
+  go get golang.org/x/sys/unix
+fi
+protoc -I server/ server/*.proto --go_out=plugins=grpc:server
+GOOS=linux GOARCH=amd64 go build github.com/edudev/chord
+cp chord remote/
+echo "Please copy the folder "\""remote"\"" onto DAS5 using scp"
+echo "Run install.sh to download:Go, Memcached, Memtier and Memslap"
