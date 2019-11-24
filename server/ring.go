212c0a89f7c51f5dbcf44b45cb11d147e8adc9a5
julihoh <julihoh@users.noreply.github.com>
2019-11-24 12:11:54 +0100

    Finger table successor (#28)
    * find successor from list of nodes
    
    Co-Authored-By: Emil Dudev <e.dudev@student.vu.nl>

--- server/ring.go
+++ server/ring.go
@@ -3,35 +3,52 @@ package server
 import (
 	"context"
 	hash "crypto/sha1"
+	"math/big"
+	"sort"
 
-	"google.golang.org/grpc"
 	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
 )
 
 const (
+	// M as it is used in the paper. M specifies the size of the identifier ring,
+	// which is 2^M in size (M specifies the amount of bits in an identifier).
+	// It is chosen to be the amount of bits we receive from the hashing function.
 	M uint = hash.Size * 8
 )
 
-type position [M/8]byte
+// a position should be treated as an opague value.
+// however, for the finger table operations, it is useful to treat it as a number.
+type position *big.Int
+
+// compares to positions / spaceship operator
+func cmpPosition(a position, b position) int {
+	return ((*big.Int)(a)).Cmp((*big.Int)(b))
+}
 
 type node struct {
 	addr address
-	pos position
+	pos  position
 }
 
 type chordRing struct {
 	server *ChordServer
 
-	myNode node
+	myNode      node
 	fingerTable [M]node
 
 	UnimplementedChordRingServer
 }
 
 func addr2node(addr address) node {
+	var pos big.Int
+	// the ordering of bytes shouldn't matter as long as it is consistent.
+	// SetBytes treats the number as unsigned.
+	hash := hash.Sum([]byte(addr))
+	pos.SetBytes(hash[:])
 	return node{
 		addr: addr,
-		pos: position(hash.Sum([]byte(addr))),
+		pos:  position(&pos),
 	}
 }
 
@@ -63,8 +80,36 @@ func (r *chordRing) getClient(addr address) (client ChordRingClient) {
 	return
 }
 
+type sortByPosition []ChordServer
+
+func (a sortByPosition) Len() int      { return len(a) }
+func (a sortByPosition) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a sortByPosition) Less(i, j int) bool {
+	return cmpPosition(a[i].ring.myNode.pos, a[j].ring.myNode.pos) < 0
+}
+
 func (r *chordRing) fillFingerTable(servers []ChordServer) {
-	// TODO: fill the first entry of the finger table (i.e. the successor)
+	sort.Sort(sortByPosition(servers))
+	foundNode := false
+	for _, s := range servers {
+		if cmpPosition(s.ring.myNode.pos, r.myNode.pos) > 0 {
+			// s is the first node that is a successor to us.
+			r.fingerTable[0] = s.ring.myNode
+			foundNode = true
+			break
+		}
+	}
+	if !foundNode {
+		// if we couldn't find a successor yet we need to wrap.
+		if (cmpPosition(servers[0].ring.myNode.pos, r.myNode.pos)) == 0 {
+			// there is no successor
+		} else {
+			// the succesor is the next element after wrapping,
+			// therefore the first element in the sorted array.
+			r.fingerTable[0] = servers[0].ring.myNode
+		}
+	}
+	// TODO: fill the rest of the table (will be done in a separate pull/commit)
 }
 
 func (r *chordRing) findSuccessor(keyPos position) (successor node) {
@@ -78,12 +123,12 @@ func (r *chordRing) findPredecessor(keyPos position) (predecessor node) {
 	return node{}
 }
 
-func (c *chordRing) GetSuccessor(ctx context.Context, in *empty.Empty) (*RPCNode, error) {
+func (r *chordRing) GetSuccessor(ctx context.Context, in *empty.Empty) (*RPCNode, error) {
 	// TODO: return the current node's successor
 	return &RPCNode{}, nil
 }
 
-func (c *chordRing) ClosestPrecedingFinger(ctx context.Context, in *LookupRequest) (*RPCNode, error) {
+func (r *chordRing) ClosestPrecedingFinger(ctx context.Context, in *LookupRequest) (*RPCNode, error) {
 	// TODO: return the successor node, for now
 	return &RPCNode{}, nil
 }
