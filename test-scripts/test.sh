a06216f9f5a4187c45b4d9169dc5cb773a30dfd3
dcasenove <d.casenove@gmail.com>
2019-11-25 11:30:59 +0100

    Clear env on prun

--- test-scripts/test.sh
+++ test-scripts/test.sh
@@ -6,7 +6,7 @@ TIMESTAMP="$(date +"%Y%m%d_%H%M%S")"
 
 module load prun
 
-MEMCACHED_EXEC="$HOME/opt/bin/memcached"
+MEMCACHED_EXEC="env -i $HOME/opt/bin/memcached"
 MEMCACHED_ARGS=""
 
 echo "Starting Memcached..."
@@ -17,13 +17,13 @@ NODE="$(grep -o -P "node.{0,3}" $HOME/tmpout)"
 echo "Memcached started on $NODE."
 echo "Output of Memcached: $HOME/memcached_out_$TIMESTAMP"
 
-MEMTIER_B_EXEC="$HOME/opt/bin/memtier_benchmark"
+MEMTIER_B_EXEC="env -i $HOME/opt/bin/memtier_benchmark"
 MEMTIER_B_ARGS="-s $NODE -p 11211 -P memcache_text -x 1"
 echo "Running Memtier Benchmark..."
 prun -np 1 $MEMTIER_B_EXEC $MEMTIER_B_ARGS </dev/null >$HOME/memtier_out_$TIMESTAMP 2>&1
 echo "Benchmark results: $HOME/memtier_out_$TIMESTAMP"
 
-MEMSLAP_EXEC="$HOME/opt/bin/memslap"
+MEMSLAP_EXEC="env -i $HOME/opt/bin/memslap"
 MEMSLAP_ARGS="--servers=$NODE"
 echo "Running Memslap..."
 prun -np 1 $MEMSLAP_EXEC $MEMSLAP_ARGS </dev/null >$HOME/memslap_out_$TIMESTAMP 2>&1
--- test-scripts/test_multiple_memcached.sh
+++ test-scripts/test_multiple_memcached.sh
@@ -5,7 +5,7 @@ TIMESTAMP="$(date +"%Y%m%d_%H%M%S")"
 
 module load prun
 
-MEMCACHED_EXEC="$HOME/opt/bin/memcached"
+MEMCACHED_EXEC="env -i $HOME/opt/bin/memcached"
 MEMCACHED_ARGS=""
 echo "Starting Memcached..."
 prun -v -np 4 $MEMCACHED_EXEC $MEMCACHED_ARGS < /dev/null >$HOME/memcached_out_$TIMESTAMP 2>$HOME/tmpout &
@@ -21,7 +21,7 @@ do
 done
 echo "Output of Memcached: $HOME/memcached_out_$TIMESTAMP"
 
-MEMSLAP_EXEC="$HOME/opt/bin/memslap"
+MEMSLAP_EXEC="env -i $HOME/opt/bin/memslap"
 MEMSLAP_ARGS="--servers=${r[0]},${r[1]},${r[2]},${r[3]}"
 echo $MEMSLAP_ARGS
 echo "Running Memslap..."
