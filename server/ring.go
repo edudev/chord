62e031e7c067a0dbd9111092323c0f07984b71c2
Julius Hohnerlein <julihoh@users.noreply.github.com>
2019-12-10 12:32:37 +0100

    fix another copy past bug

--- server/ring.go
+++ server/ring.go
@@ -398,7 +398,7 @@ func wrapNextFingerFixIndex(k uint) uint {
 
 // sets the nextSuccessorFixIndex to the next value and returns the old one
 func (r *chordRing) rotateNextSuccessorFixIndex() (k uint) {
-	k = r.nextFingerFixIndex
+	k = r.nextSuccessorFixIndex
 	r.nextSuccessorFixIndex = (k + 1) % R
 	return
 }
